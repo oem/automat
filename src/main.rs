2ceafccd4b95ee975963e41337d63424e641f8fd
Ömür Özkir <oemuer.oezkir@gmail.com>
2020-06-14 17:17:10 +0200

    doing some warmup exercise, reading stdin piped into this process

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
+use std::io::{self, BufRead};
+use structopt::StructOpt;
+
+#[derive(StructOpt)]
+struct Cli {
+    cmd: String,
+    #[structopt(parse(from_os_str))]
+    file: std::path::PathBuf,
+}
+
+fn main() -> io::Result<()> {
+    let args = Cli::from_args();
     println!("exploratory data analysis via the command line");
+    println!("running {} on file {:?}", args.cmd, args.file);
+
+    let stdin = io::stdin();
+    for line in stdin.lock().lines() {
+        println!("{}", line.unwrap());
+    }
+    Ok(())
 }
