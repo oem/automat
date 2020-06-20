b2795195923feef82e74ed18d7f9a3d82db4b695
Ömür Özkir <oemuer.oezkir@gmail.com>
2020-06-20 18:26:42 +0200

    start implementing first feature, filtering columns

--- src/main.rs
+++ src/main.rs
@@ -7,14 +7,18 @@ use structopt::StructOpt;
 #[derive(StructOpt)]
 struct Cli {
     cmd: String,
+    conditions: String,
     #[structopt(parse(from_os_str))]
-    file: path::PathBuf,
+    input: path::PathBuf,
 }
 
 fn main() -> io::Result<()> {
     let args = Cli::from_args();
     println!("exploratory data analysis via the command line");
-    println!("running {} on file {:?}", args.cmd, args.file);
+    println!(
+        "cmd {}, conditions {}, input {:?}",
+        args.cmd, args.conditions, args.input
+    );
 
     let stdin = io::stdin();
     for line in stdin.lock().lines() {
