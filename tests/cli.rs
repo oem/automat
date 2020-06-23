ec16c11eb47aadd1bce1594d12cc2b14e2014d40
Ömür Özkir <oemuer.oezkir@gmail.com>
2020-06-23 22:25:46 +0200

    add regression tests for success/failure scenarios for filter cmd

--- tests/cli.rs
+++ tests/cli.rs
@@ -6,7 +6,16 @@ use std::process::Command;
 fn filter_without_conditions() -> Result<(), Box<dyn std::error::Error>> {
     let mut cmd = Command::cargo_bin("atm")?;
     cmd.arg("filter");
-    cmd.assert().failure();
+    cmd.assert()
+        .failure()
+        .stderr(predicate::str::contains("conditions"));
+    Ok(())
+}
 
+#[test]
+fn filter_with_conditions() -> Result<(), Box<dyn std::error::Error>> {
+    let mut cmd = Command::cargo_bin("atm")?;
+    cmd.arg("filter").arg("foo<12");
+    cmd.assert().success();
     Ok(())
 }
