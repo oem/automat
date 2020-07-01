acb576fe9b4a4e9f0364fb8b14c554c0db462e9c
Ömür Özkir <oemuer.oezkir@gmail.com>
2020-07-01 22:51:59 +0200

    It's time to remove some of the hard-coded stuff, first order: parse condition

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 extern crate csv;
 
 use std::fs::File;
+use std::io;
 use std::path::PathBuf;
 use std::str::FromStr;
 use structopt::StructOpt;
@@ -29,20 +30,36 @@ enum Command {
     },
 }
 
-fn filter<R: std::io::Read>(
+fn filter<R: io::Read>(
     mut rdr: csv::Reader<R>,
-    value: f32,
-    col_index: usize,
+    condition: String,
 ) -> Result<Vec<csv::StringRecord>, Box<dyn std::error::Error>> {
     let mut rows: Vec<csv::StringRecord> = vec![];
     for result in rdr.records() {
         let record = result?;
-        let col = f32::from_str(&record[col_index])?;
-        if col > value {
+        let col = f32::from_str(&record[1])?; // magic number one: col_index from condition
+        if col > 12.0 {
+            // ^ magic number two: value from condition, also operation
             rows.push(record);
         }
     }
     Ok(rows)
+}
+
+#[derive(Debug, PartialEq)]
+enum Check {
+    GreaterThan(f32),
+    SmallerThan(f32),
+    GreaterThanOrEqual(f32),
+    SmallerThanOrEqual(f32),
+}
+
+fn get_condition_parts<R: io::Read + std::fmt::Debug>(
+    rdr: csv::Reader<R>,
+    condition: String,
+) -> (usize, Check) {
+    println!("{:?} {}", rdr, condition);
+    todo!()
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -50,12 +67,12 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     match opt.input {
         Some(input) => {
             let file = File::open(input)?;
-            let filtered = filter(csv::Reader::from_reader(file), 12.0, 1)?;
+            let filtered = filter(csv::Reader::from_reader(file), "id>12".to_string())?;
             println!("{:?}", filtered);
             Ok(())
         }
         None => {
-            let filtered = filter(csv::Reader::from_reader(std::io::stdin()), 12.0, 1)?;
+            let filtered = filter(csv::Reader::from_reader(io::stdin()), "id>12".to_string())?;
             println!("{:?}", filtered);
             Ok(())
         }
@@ -70,8 +87,17 @@ mod tests {
     fn filter_larger_than() {
         let csv = "name,id\nmoo,12\nfoo,42";
         let rdr = csv::Reader::from_reader(csv.as_bytes());
-        let filtered = filter(rdr, 12.0, 1).unwrap();
+        let filtered = filter(rdr, "id>12".to_string()).unwrap();
 
         assert_eq!(filtered, vec![csv::StringRecord::from(vec!["foo", "42"])])
+    }
+
+    #[test]
+    fn test_get_condition_parts() {
+        let csv = "name,id\nmoo,12\nfoo,42";
+        let rdr = csv::Reader::from_reader(csv.as_bytes());
+        let parts = get_condition_parts(rdr, "id>12".to_string());
+        assert_eq!(parts.0, 1);
+        assert_eq!(parts.1, Check::GreaterThan(12.0));
     }
 }
