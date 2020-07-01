1fd8d9fab46b84f753d16508bde8b45206eb56d0
Ömür Özkir <oemuer.oezkir@gmail.com>
2020-07-01 17:55:09 +0200

    let's go with a very naive approach first, we will still hard code everything but implement filtering of columns. First, parse column value

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ extern crate csv;
 
 use std::fs::File;
 use std::path::PathBuf;
+use std::str::FromStr;
 use structopt::StructOpt;
 
 #[derive(Debug, StructOpt)]
@@ -30,12 +31,16 @@ enum Command {
 
 fn filter<R: std::io::Read>(
     mut rdr: csv::Reader<R>,
+    value: f32,
 ) -> Result<csv::StringRecord, Box<dyn std::error::Error>> {
+    println!("{:?}", rdr.headers()?);
     for result in rdr.records() {
         let record = result?;
+        let col = f32::from_str(&record[1])?;
+        println!("col: {}", col);
         println!("{:?}", record);
     }
-    Ok(csv::StringRecord::from(vec!["42", "dog", "56"]))
+    Ok(csv::StringRecord::from(vec!["foo", "42"]))
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -43,11 +48,11 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     match opt.input {
         Some(input) => {
             let file = File::open(input)?;
-            filter(csv::Reader::from_reader(file))?;
+            filter(csv::Reader::from_reader(file), 12.0)?;
             Ok(())
         }
         None => {
-            filter(csv::Reader::from_reader(std::io::stdin()))?;
+            filter(csv::Reader::from_reader(std::io::stdin()), 12.0)?;
             Ok(())
         }
     }
@@ -59,12 +64,12 @@ mod tests {
 
     #[test]
     fn filter_larger_than() {
-        let csv = "id,moo,points
-            12,cow,33
-            42,dog,56";
+        let csv = "name,id
+            moo,12
+            foo,42";
         let rdr = csv::Reader::from_reader(csv.as_bytes());
-        let filtered = filter(rdr).unwrap();
+        let filtered = filter(rdr, 12.0).unwrap();
 
-        assert_eq!(filtered, csv::StringRecord::from(vec!["42", "dog", "56"]))
+        assert_eq!(filtered, csv::StringRecord::from(vec!["foo", "42"]))
     }
 }
