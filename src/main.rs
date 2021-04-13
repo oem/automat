85ca6186ed1570b551e64dd03c1c465dd72c0e54
Jonathan Strong <jonathan.strong@gmail.com>
2021-04-13 12:45:45 -0400

    handle pipe errors gracefully
    before, atm would panic when something upstream of it ended output early:
    
      $ cat my-data.csv | atm filter 'col < 60' | xsv table | head
      time                       col
      2020-08-15T00:00:00-04:00  18.523435
      2020-08-15T00:05:00-04:00  18.19156
      2020-08-15T00:10:00-04:00  18.19156
      2020-08-15T00:15:00-04:00  19.45906
      2020-08-15T00:20:00-04:00  18.591977
    
      thread 'main' panicked at 'failed printing to stdout: Broken pipe (os error 32)', library/std/src/io/stdio.rs:940:9
      note: run with `RUST_BACKTRACE=1` environment variable to display a backtrace
    
    this change checks the `io::ErrorKind` and ignores `io::ErrorKind::BrokenPipe` (converts it to `Ok(())`)

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ mod cmd;
 
 use csv::Reader;
 use std::error::Error;
-use std::io;
+use std::io::{self, Write};
 use std::path::PathBuf;
 use structopt::StructOpt;
 
@@ -31,6 +31,23 @@ enum Command {
     },
 }
 
+/// this prevents a panic when output is ended prematurely, e.g. by piping to `head`.
+fn swallow_pipe_error(res: Result<(), io::Error>) -> Result<(), io::Error> {
+    match res {
+        Ok(()) => Ok(()),
+        Err(e) if e.kind() == io::ErrorKind::BrokenPipe => Ok(()),
+        Err(other) => Err(other),
+    }
+}
+
+fn print_table(filtered: Vec<csv::StringRecord>) -> Result<(), io::Error> {
+    let mut stdout = io::BufWriter::new(io::stdout());
+    for row in filtered {
+        writeln!(&mut stdout, "{}", row.iter().collect::<Vec<&str>>().join(","))?;
+    }
+    Ok(())
+}
+
 fn main() -> Result<(), Box<dyn Error>> {
     let opt = Opt::from_args();
     let filter_condition = match opt.cmd {
@@ -40,22 +57,16 @@ fn main() -> Result<(), Box<dyn Error>> {
     // remove whitespace
     let filter_condition: String = filter_condition.replace(" ", "");
 
-    fn print_table(filtered: Vec<csv::StringRecord>) {
-        for row in filtered {
-            println!("{}", row.iter().collect::<Vec<&str>>().join(","));
-        }
-    }
-
     match opt.input {
         Some(input) => {
             let filtered = cmd::filter(Reader::from_path(input)?, filter_condition.as_str())?;
-            print_table(filtered);
+            swallow_pipe_error(print_table(filtered))?;
             Ok(())
         }
         None => {
             let filtered =
                 cmd::filter(Reader::from_reader(io::stdin()), filter_condition.as_str())?;
-            print_table(filtered);
+            swallow_pipe_error(print_table(filtered))?;
             Ok(())
         }
     }
